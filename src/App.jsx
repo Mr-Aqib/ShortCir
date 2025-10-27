ce2b7c26b5e9d8a40e2fb6a966bd497656b68286
Mr_Aqib <mraqibshafique@gmail.com>
2025-10-27 20:22:35 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Useeffect from "./Hooks/Useeffect";
-import Apifetch from "./Api Req/Apifetch";
+import Apifetch from "./ApiReq/Apifetch";
 import ControlledInputs from "./Inputs/ControlledInputs";
 import { Toaster } from "react-hot-toast";
-
 import Birthday from "./Birthday Reminder/Birthday";
 import Event from "./Events/Event";
 import Us from "./Hooks/Us";
@@ -13,35 +12,35 @@ import Todo from "./TodoList/Todo";
 import Accordians from "./Accordians/Accordians";
 import Productmain from "./ProductAPI/Productmain";
 import Form from "./Form/Form";
+import ColorGenerator from "./Colorgenerator/ColorGenerator";
 import UseRef from "./Hooks/UseRef";
 import Mainpage from "./Website/Mainpage";
 import UsereducerTodo from "./TodoList/UsingRefcomplexTodo/UsereducerTodo";
 import PropDrilling from "./Hooks/UseContext/PropDrilling";
-// import TextGenerator from "./TextGenerator/TextGenerator";
+import TextGenerator from "./TextGenerator/TextGenerator";
 
 const App = () => {
   return (
     <>
-      {
-        <Toaster />
-        /*<Event/>
-    <Us/>
-    <Counter/>
-    <ShortCitcuit/> 
-    <Birthday/> 
-    <Useeffect/> 
-    <Apifetch/>
-    <ControlledInputs/> */
-      }
-      {/* <Todo/> */}
-      {/* <TextGenerator /> */}
-      {/* <ColorGenerator/> */}
-      {/* <Accordians /> */}
-      {/* <Productmain /> */}
-      {/* <Form /> */}
-      {/* <UseRef /> */}
-      {/* <Mainpage /> */}
-      {/* <UsereducerTodo /> */}
+      <Toaster />
+      <Event />
+      <Us />
+      <Counter />
+      <ShortCitcuit />
+      <Birthday />
+      <Useeffect />
+      <Apifetch />
+      <ControlledInputs />
+
+      <Todo />
+      <TextGenerator />
+      <ColorGenerator />
+      <Accordians />
+      <Productmain />
+      <Form />
+      <UseRef />
+      <Mainpage />
+      <UsereducerTodo />
       <PropDrilling />
     </>
   );
