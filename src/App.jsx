2d9374210db91b7faefc963604908b384039cd23
Mr_Aqib <mraqibshafique@gmail.com>
2025-10-27 19:07:00 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Us from "./Hooks/Us";
 import Counter from "./Counterapp/Counter";
 import ShortCitcuit from "./ShortCircuits/ShortCircuit";
 import Todo from "./TodoList/Todo";
-import TextGenerator from "./TextGenerator/TextGenerator";
 import Accordians from "./Accordians/Accordians";
 import Productmain from "./ProductAPI/Productmain";
 import Form from "./Form/Form";
@@ -18,6 +17,7 @@ import UseRef from "./Hooks/UseRef";
 import Mainpage from "./Website/Mainpage";
 import UsereducerTodo from "./TodoList/UsingRefcomplexTodo/UsereducerTodo";
 import PropDrilling from "./Hooks/UseContext/PropDrilling";
+import TextGenerator from "./TextGenerator/TextGenerator";
 
 const App = () => {
   return (
